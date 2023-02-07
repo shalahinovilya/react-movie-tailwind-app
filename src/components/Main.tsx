4a19a0233f305a765d0fa7a18543c1d06c935d39
ilya <shalaginovilya703@gmail.com>
2023-02-07 11:26:54 +0200

    Modal component was created for video playing

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,59 +1,72 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {category, useGetMovieListQuery} from "../store/services/watchTvService";
 import Loader from "./Loader";
-import {Link} from "react-router-dom";
+import VideoModal from "./common/VideoModal";
+import {CommonType} from "../types/CommonType";
 
 const random = (min: number, max: number) => Math.floor(Math.random() * (max - min) + min)
 
 const Main = () => {
 
-    let item
+    const [showModal, setShowModal] = useState(false)
+    const [currentItem, setCurrentItem] = useState<CommonType>()
+    const handleShowModal = () => setShowModal(!showModal)
+
     const {data = {results: []}, isLoading, isError} = useGetMovieListQuery({
         cat: category.movie,
         page: 1,
         type: 'popular'
-    })
+    }, )
+
+    useEffect(() => {
+        if (!data) return;
+        setCurrentItem(data.results[random(0, data.results?.length || 20)]);
+    }, [data]);
+
+
+    let item
 
     if (isLoading) return (
         <Loader/>
     )
 
-    if (!data || isError) {
+    if (!data || isError || !currentItem) {
         return <div className="h-40"></div>
     }
 
-    item = data.results[random(0, data.results?.length || 20)]
-
     return (
-        <div className="h-[700px] w-full relative bg-amber-50">
-            <div className="h-full w-full absolute bg-gradient-to-b from-black"></div>
-            <img
-                className="h-full w-full object-cover"
-                src={`https://image.tmdb.org/t/p/original/${item.backdrop_path}`}
-                alt={item.backdrop_path}/>
-            <div className="absolute top-[25%] p-4">
-                <h1 className="text-white text-5xl font-bold">
-                    {item.original_title}
-                </h1>
-                <div className="mt-7">
-                    <Link to={`/${category.movie}/${item.id}`}>
-                        <button className="border text-black text-3xl px-5 py-2 mr-4 mb-4 bg-gray-300">
+        <>
+            <VideoModal isVideoOpen={showModal} videoUrl={''} toggleVideoModal={handleShowModal}/>
+            <div className="h-[700px] w-full relative bg-amber-50">
+                <div className="h-full w-full absolute bg-gradient-to-b from-black"></div>
+                <img
+                    className="h-full w-full object-cover"
+                    src={`https://image.tmdb.org/t/p/original/${currentItem.backdrop_path}`}
+                    alt={currentItem.backdrop_path}/>
+                <div className="absolute top-[25%] p-4">
+                    <h1 className="text-white text-5xl font-bold">
+                        {currentItem.original_title}
+                    </h1>
+                    <div className="mt-7">
+                        <button
+                            onClick={handleShowModal}
+                            className="border text-black text-3xl px-5 py-2 mr-4 mb-4 bg-gray-300">
                             Play
                         </button>
-                    </Link>
-                    <button className="border text-white text-3xl px-5 py-2 border-white hover:bg-gray-300
-                        hover:text-black transition-colors duration-500 ease-in-out">
-                        Watch Later
-                    </button>
+                        <button className="border text-white text-3xl px-5 py-2 border-white hover:bg-gray-300
+                            hover:text-black transition-colors duration-500 ease-in-out">
+                            Watch Later
+                        </button>
+                    </div>
+                    <time className="text-gray-300 text-1xl">
+                        {currentItem.release_date}
+                    </time>
+                    <p className="w-full text-white text-2xl md:max-w-[70%] lg:max-w-[50%] xl:max-w-[40%]">
+                        {currentItem.overview}
+                    </p>
                 </div>
-                <time className="text-gray-300 text-1xl">
-                    {item.release_date}
-                </time>
-                <p className="w-full text-white text-2xl md:max-w-[70%] lg:max-w-[50%] xl:max-w-[40%]">
-                    {item.overview}
-                </p>
             </div>
-        </div>
+        </>
     );
 };
 
---
+++ src/components/common/VideoModal.tsx
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import {IoMdClose} from "react-icons/io";
+import ReactPlayer from "react-player";
+
+interface VideoModalProps {
+    isVideoOpen: boolean;
+    videoUrl: string;
+    toggleVideoModal: () => void;
+}
+
+const VideoModal = ({isVideoOpen, videoUrl, toggleVideoModal} : VideoModalProps) => {
+
+    const [shouldPauseVideo, setShouldPauseVideo] = useState(false);
+
+    return (
+        <div
+            className={`${isVideoOpen ? 'flex' : 'hidden'} 
+                        ${isVideoOpen ? 'opacity-100' : 'opacity-0'} 
+                        flex justify-center fixed h-screen w-screen z-[1000] top-0 bg-black/90`}
+            onClick={() => {
+                toggleVideoModal()
+                setShouldPauseVideo(true)
+            }}
+        >
+            <button
+                onClick={toggleVideoModal}
+                className="absolute right-0 mt-25 mr-25 scale-110 z-[1500] opacity-80 hover:opacity-100 hover:cursor-pointer"
+            >
+                <IoMdClose/>
+            </button>
+            <div className="ml-2 mr-2 flex items-center justify-center h-[100%]">
+                <div className="rounded-2xl border-2 flex flex-col overflow-hidden">
+                    <div onClick={e => {
+                        e.stopPropagation()
+                        setShouldPauseVideo(true)
+                    }}
+                         className="mt-5 h-0 min-w-[70vw] pb-[56.25%] relative">
+                        <div className="absolute top-0 left-0 overflow-hidden w-[100%] h-[100%]">
+                            <ReactPlayer
+                                width='100%'
+                                height='100%'
+                                playing={!shouldPauseVideo}
+                                onPlay={() => setShouldPauseVideo(false)}
+                                onInterrupt={() => setShouldPauseVideo(true)}
+                                url={`https://www.youtube.com/watch?v=6JnN1DmbqoU`}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default VideoModal;
